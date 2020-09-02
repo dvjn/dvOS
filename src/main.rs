f7fdca2e4802c424861a767a6c2e6d5a4b000947
Divy Jain <dkj@somaiya.edu>
2020-09-02 16:18:09 +0530

    🎉 initial setup

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+extern crate rlibc;
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    HELLO.iter().enumerate().for_each(|(i, &byte)| unsafe {
+        *vga_buffer.offset(i as isize * 2) = byte;
+        *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    });
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
