094bba0482761010acf4ecaff6935f4e1c01481b
Divy Jain <dkj@somaiya.edu>
2020-09-02 21:34:46 +0530

    ✨ add vga buffer print macros

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,28 @@
 #![no_main]
 
 extern crate rlibc;
-use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    HELLO.iter().enumerate().for_each(|(i, &byte)| unsafe {
-        *vga_buffer.offset(i as isize * 2) = byte;
-        *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    });
+    println!("Hello World, I can write.");
+    println!();
+    println!("I can write number: {} ! ", 123);
+    println!("I can write float: {} ! ", 1.0 / 2.0);
+    println!("I can write bool: {} ! ", true);
+    println!("I cannot write emoji: {} ! :(", "😭");
+    println!();
+    println!("I can wrap a very very very very very very very very very very very very very very very long line!");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("Panicked: {}", info);
+
     loop {}
 }
