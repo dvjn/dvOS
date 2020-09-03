c69c550c20ebaec0e508c7980b0603c9188e29c2
Divy Jain <dkj@somaiya.edu>
2020-09-03 20:14:35 +0530

    ⚡️ update println output test

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -172,10 +172,16 @@ fn test_println_many() {
 fn test_println_output() {
     let s = "Some test string that fits on a single line";
     println!("{}", s);
-    for (i, c) in s.chars().enumerate() {
-        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-        assert_eq!(char::from(screen_char.ascii_character), c);
-    }
+    s.chars().enumerate().for_each(|(i, c)| {
+        assert_eq!(
+            char::from(
+                WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i]
+                    .read()
+                    .ascii_character
+            ),
+            c
+        );
+    });
 }
 
 #[test_case]
