41ae5dcfb360cbe3adb90495a60c1b6b873ea7e2
Divy Jain <dkj@somaiya.edu>
2021-01-28 17:22:47 +0530

    🏗 seperate testing entrypoint function

--- src/main.rs
+++ src/main.rs
@@ -11,101 +11,102 @@ use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
+#[cfg(not(test))]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     dv_os::init();
 
-    #[cfg(not(test))]
-    {
-        use alloc::{boxed::Box, rc::Rc, vec::Vec};
-        use dv_os::{
-            allocator, color_code, colored_print, memory, memory::BootInfoFrameAllocator, println,
-            Color,
-        };
-        use x86_64::VirtAddr;
+    use alloc::{boxed::Box, rc::Rc, vec::Vec};
+    use dv_os::{
+        allocator, color_code, colored_print, memory, memory::BootInfoFrameAllocator, println,
+        Color,
+    };
+    use x86_64::VirtAddr;
 
-        let number = 123;
-        let float = 1.0 / 2.0;
-        let boolean = true;
-        let emoji = "😭";
+    let number = 123;
+    let float = 1.0 / 2.0;
+    let boolean = true;
+    let emoji = "😭";
 
-        println!("Hello World, I can write evrything!");
-        println!(
-            "Integer: {}, Float: {}, Boolean: {}, Unknown: {}",
-            number, float, boolean, emoji
-        );
+    println!("Hello World, I can write evrything!");
+    println!(
+        "Integer: {}, Float: {}, Boolean: {}, Unknown: {}",
+        number, float, boolean, emoji
+    );
 
-        let colors = [
-            Color::Black,
-            Color::Blue,
-            Color::Green,
-            Color::Cyan,
-            Color::Red,
-            Color::Magenta,
-            Color::Brown,
-            Color::LightGray,
-            Color::DarkGray,
-            Color::LightBlue,
-            Color::LightGreen,
-            Color::LightCyan,
-            Color::LightRed,
-            Color::Pink,
-            Color::Yellow,
-            Color::White,
-        ];
+    let colors = [
+        Color::Black,
+        Color::Blue,
+        Color::Green,
+        Color::Cyan,
+        Color::Red,
+        Color::Magenta,
+        Color::Brown,
+        Color::LightGray,
+        Color::DarkGray,
+        Color::LightBlue,
+        Color::LightGreen,
+        Color::LightCyan,
+        Color::LightRed,
+        Color::Pink,
+        Color::Yellow,
+        Color::White,
+    ];
 
-        println!("I can write in colorful too! :)");
-        for fg in colors.iter() {
-            for bg in colors.iter() {
-                colored_print!(color_code!(*fg, *bg), " dv ")
-            }
-            println!();
+    println!("I can write in colorful too! :)");
+    for fg in colors.iter() {
+        for bg in colors.iter() {
+            colored_print!(color_code!(*fg, *bg), " dv ")
         }
         println!();
-
-        println!("And here comes a breakpoint interrupt...");
-        x86_64::instructions::interrupts::int3();
-        colored_print!(
-            color_code!(Color::Green),
-            "Interrupt was handled, and it did not crash!"
-        );
-        println!();
-
-        let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-        let mut mapper = unsafe { memory::init(phys_mem_offset) };
-        let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-        allocator::init_heap(&mut mapper, &mut frame_allocator)
-            .expect("heap initialization failed");
-
-        let heap_value = Box::new(69);
-        println!("heap_value at {:p}", heap_value);
-
-        let mut heap_vector = Vec::new();
-        for i in 0..500 {
-            if i % 50 == 0 {
-                println!(
-                    "with {:0>3} values, heap_vector at {:p}",
-                    i,
-                    heap_vector.as_slice(),
-                );
-            }
-            heap_vector.push(i);
-        }
-        println!(
-            "with 500 values, heap_vector at {:p}",
-            heap_vector.as_slice()
-        );
-
-        let reference_counted = Rc::new(heap_vector);
-        let cloned_reference = reference_counted.clone();
-        println!("Ref count is {}", Rc::strong_count(&cloned_reference));
-        core::mem::drop(reference_counted);
-        println!("New ref count is {}", Rc::strong_count(&cloned_reference));
     }
+    println!();
 
-    #[cfg(test)]
+    println!("And here comes a breakpoint interrupt...");
+    x86_64::instructions::interrupts::int3();
+    colored_print!(
+        color_code!(Color::Green),
+        "Interrupt was handled, and it did not crash!"
+    );
+    println!();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(69);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut heap_vector = Vec::new();
+    for i in 0..500 {
+        if i % 50 == 0 {
+            println!(
+                "with {:0>3} values, heap_vector at {:p}",
+                i,
+                heap_vector.as_slice(),
+            );
+        }
+        heap_vector.push(i);
+    }
+    println!(
+        "with 500 values, heap_vector at {:p}",
+        heap_vector.as_slice()
+    );
+
+    let reference_counted = Rc::new(heap_vector);
+    let cloned_reference = reference_counted.clone();
+    println!("Ref count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("New ref count is {}", Rc::strong_count(&cloned_reference));
+
+    dv_os::hlt_loop();
+}
+
+#[cfg(test)]
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+    dv_os::init();
     test_main();
-
     dv_os::hlt_loop();
 }
 
