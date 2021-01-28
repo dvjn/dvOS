013a6dab0b14d7238cbf9c3867f77b22d6eeaf09
Divy Jain <dkj@somaiya.edu>
2021-01-28 20:20:28 +0530

    ✨ add simple task executor

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,19 @@ extern crate alloc;
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use dv_os::task::{simple_executor::SimpleExecutor, Task};
 
 entry_point!(kernel_main);
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    use dv_os::println;
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
 #[cfg(not(test))]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
@@ -22,17 +33,20 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     };
     use x86_64::VirtAddr;
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
     let number = 123;
     let float = 1.0 / 2.0;
     let boolean = true;
     let emoji = "😭";
-
     println!("Hello World, I can write evrything!");
     println!(
         "Integer: {}, Float: {}, Boolean: {}, Unknown: {}",
         number, float, boolean, emoji
     );
-
     let colors = [
         Color::Black,
         Color::Blue,
@@ -51,7 +65,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         Color::Yellow,
         Color::White,
     ];
-
     println!("I can write in colorful too! :)");
     for fg in colors.iter() {
         for bg in colors.iter() {
@@ -60,7 +73,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         println!();
     }
     println!();
-
     println!("And here comes a breakpoint interrupt...");
     x86_64::instructions::interrupts::int3();
     colored_print!(
@@ -69,15 +81,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     );
     println!();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
-
     let heap_value = Box::new(69);
     println!("heap_value at {:p}", heap_value);
-
     let mut heap_vector = Vec::new();
     for i in 0..500 {
         if i % 50 == 0 {
@@ -93,12 +98,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         "with 500 values, heap_vector at {:p}",
         heap_vector.as_slice()
     );
-
     let reference_counted = Rc::new(heap_vector);
     let cloned_reference = reference_counted.clone();
     println!("Ref count is {}", Rc::strong_count(&cloned_reference));
     core::mem::drop(reference_counted);
     println!("New ref count is {}", Rc::strong_count(&cloned_reference));
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
 
     dv_os::hlt_loop();
 }
