ca1b7f3a5d484f92880529dac542b7e1c38bcd51
Divy Jain <dkj@somaiya.edu>
2021-01-28 21:22:28 +0530

    ✨ add keyboard task executor

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     {
         use dv_os::{
             println,
-            task::{simple_executor::SimpleExecutor, Task},
+            task::{keyboard, simple_executor::SimpleExecutor, Task},
         };
 
         async fn async_number() -> u32 {
@@ -139,6 +139,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
         let mut executor = SimpleExecutor::new();
         executor.spawn(Task::new(example_task()));
+        executor.spawn(Task::new(keyboard::print_keypresses()));
         executor.run();
     }
 
